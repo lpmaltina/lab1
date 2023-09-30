df791f7aa6dfcb9dc225847f5ba7695d58e1c23f
lpmaltina <lyudmilamaltina@gmail.com>
2023-09-30 23:18:23 +0300

    Generate data and do timings for serial realization

---
+++ generate-input.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int generateInt(int min, int max, int step){
+    return min + rand() % ((max - min) / step + 1) * step;
+}
+
+void generateInput(int bodies, int timeSteps){
+    int minCoord = -bodies * bodies;
+    int maxCoord = bodies * bodies;
+    int coordStep = 1;
+    int minMass = 100000;
+    int maxMass = 10000000;
+    int massStep = 100000;
+    int minSpeed = -100;
+    int maxSpeed = 100;
+    int speedStep = 1;
+    char fileName[30];
+    sprintf(fileName, "input/input-%d-%d", bodies, timeSteps);
+
+    FILE *fp = fopen(fileName, "w");
+    fprintf(fp, "6.6743e-11 %d %d\n", bodies, timeSteps);
+
+    for (int i = 0; i < bodies; ++i){
+        fprintf(
+            fp, "%d\n%f %f\n%f %f\n",
+            generateInt(minMass, maxMass, massStep),
+            (double) generateInt(minCoord, maxCoord, coordStep),
+            (double) generateInt(minCoord, maxCoord, coordStep),
+            (double) generateInt(minSpeed, maxSpeed, speedStep),
+            (double) generateInt(minSpeed, maxSpeed, speedStep)
+            );
+    }
+
+    fclose(fp);
+}
+
+int main(){
+    srand(time(NULL));
+    for (int bodies = 10; bodies <= 1000; bodies = bodies * 10){
+        for (int timeSteps = 10; timeSteps <= 1000; timeSteps = timeSteps * 10){
+            generateInput(bodies, timeSteps);
+        }
+    }
+    return 0;
+}
---
+++ lab1-serial.c
@@ -0,0 +1,162 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "timer.h"
+
+#define DT 0.05
+
+typedef struct
+{
+    double x, y;
+} vector;
+
+double start = 0;
+double end = 0;
+int bodies, timeSteps;
+double *masses, GravConstant;
+vector *positions, *velocities, *accelerations;
+
+vector addVectors(vector a, vector b)
+{
+    vector c = {a.x + b.x, a.y + b.y};
+
+    return c;
+}
+
+vector scaleVector(double b, vector a)
+{
+    vector c = {b * a.x, b * a.y};
+
+    return c;
+}
+
+vector subtractVectors(vector a, vector b)
+{
+    vector c = {a.x - b.x, a.y - b.y};
+
+    return c;
+}
+
+double mod(vector a)
+{
+    return sqrt(a.x * a.x + a.y * a.y);
+}
+
+void initiateSystem(char *fileName)
+{
+    int i;
+    FILE *fp = fopen(fileName, "r");
+
+    fscanf(fp, "%lf%d%d", &GravConstant, &bodies, &timeSteps);
+
+    masses = (double *)malloc(bodies * sizeof(double));
+    positions = (vector *)malloc(bodies * sizeof(vector));
+    velocities = (vector *)malloc(bodies * sizeof(vector));
+    accelerations = (vector *)malloc(bodies * sizeof(vector));
+
+    for (i = 0; i < bodies; i++)
+    {
+        fscanf(fp, "%lf", &masses[i]);
+        fscanf(fp, "%lf%lf", &positions[i].x, &positions[i].y);
+        fscanf(fp, "%lf%lf", &velocities[i].x, &velocities[i].y);
+    }
+
+    fclose(fp);
+}
+
+void resolveCollisions()
+{
+    int i, j;
+
+    for (i = 0; i < bodies - 1; i++)
+        for (j = i + 1; j < bodies; j++)
+        {
+            if (positions[i].x == positions[j].x && positions[i].y == positions[j].y)
+            {
+                vector temp = velocities[i];
+                velocities[i] = velocities[j];
+                velocities[j] = temp;
+            }
+        }
+}
+
+void computeAccelerations()
+{
+    int i, j;
+
+    for (i = 0; i < bodies; i++)
+    {
+        accelerations[i].x = 0;
+        accelerations[i].y = 0;
+        for (j = 0; j < bodies; j++)
+        {
+            if (i != j)
+            {
+                accelerations[i] = addVectors(accelerations[i], scaleVector(GravConstant * masses[j] / pow(mod(subtractVectors(positions[i], positions[j])), 3), subtractVectors(positions[j], positions[i])));
+            }
+        }
+    }
+}
+
+void computeVelocities()
+{
+    int i;
+
+    for (i = 0; i < bodies; i++)
+        velocities[i] = addVectors(velocities[i], scaleVector(DT, accelerations[i]));
+}
+
+void computePositions()
+{
+    int i;
+
+    for (i = 0; i < bodies; i++)
+        positions[i] = addVectors(positions[i], scaleVector(DT,velocities[i]));
+}
+
+void simulate()
+{
+    computeAccelerations();
+    computePositions();
+    computeVelocities();
+    resolveCollisions();
+}
+
+int main()
+{
+    int i, j;
+    double time;
+    int bodies, timeSteps;
+    char inputFile[30] = {0};
+    char outputFile[30] = {0};
+    for (bodies = 10; bodies <= 1000; bodies = bodies * 10){
+        for (timeSteps = 10; timeSteps <= 1000; timeSteps = timeSteps * 10){
+            time = 0;
+            sprintf(inputFile, "input/input-%d-%d", bodies, timeSteps);
+            initiateSystem(inputFile);
+            sprintf(outputFile, "output/output-serial-%d-%d", bodies, timeSteps);
+            FILE *f = fopen(outputFile, "a");
+            fprintf(f, "t\t");
+            for (j = 1; j < bodies + 1; ++j){
+                fprintf(f, "x%d\ty%d\t", j, j);
+            }
+            fprintf(f, "\n");
+
+            for (i = 0; i < timeSteps; i++)
+            {
+                GET_TIME(start);
+                simulate();
+                GET_TIME(end);
+                time += end - start;
+                fprintf(f, "%d\t", i + 1);
+                for (j = 0; j < bodies; j++){
+                    fprintf(f, "%f\t%f\t", positions[j].x, positions[j].y);
+                }
+                fprintf(f, "\n");
+            }
+            fclose(f);
+            printf("bodies=%d, timeSteps=%d: %f\n", bodies, timeSteps, time);
+        }
+    }
+    return 0;
+}
