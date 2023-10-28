8fe38eb00184125c4e2aeaeeb567a6820ee10182
NikolayKuraga <kolya54686579@yandex.ru>
2023-10-28 03:20:19 +0300

    upd dots distribution
    Now dots are distributed in more clever way if number of threads doesn't exceed number of dots (program works, but might work in not optimised way). Changed double to long double again.

--- lab1-parallel-ku.c
+++ lab1-parallel-ku.c
@@ -6,7 +6,8 @@
 #define FILE_OUTPUT "output/output-parallel-ku-10-10"
 
 
-int thread_count = 7;
+int thread_count = 800;
+double edgesPerThread = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_barrier_t barrier = {0};
@@ -81,13 +82,83 @@ void* routine(void* arg)
 	int j;
 
 	long long nth = (long long) arg;
-	int bgg = nth * (bodies / thread_count);
-	int end = 0;
-	if (thread_count - 1 != nth) {
-		end = bgg + (bodies / thread_count);
+	int bgg;
+	int end;
+
+	/* int bgg = nth * (bodies / thread_count); */
+	/* int end = 0; */
+	/* if (thread_count - 1 != nth) { */
+		/* end = bgg + (bodies / thread_count); */
+	/* } else { */
+		/* end = bodies; */
+	/* } */
+
+/* How many bodies per thread do we need? In the best case, every       */
+/* thread shall process approximately equal number of edges between.    */
+/* vertices. Number of edges between vertices is n * (n - 1) / 2.       */
+/* */
+/* bodies * (bodies - 1) / 2 = edges                                    */
+/* (1) edgesPerThread = edges / thread_count  =>                        */
+/*  =>  edgesThreadFst = edges / thread_count, however                  */
+/* */
+/* edgesThreadFst = vertsThreadFst * (vertsThreadFst - 1) / 2     =>    */
+/*  =>  vertsThreadFst^2 - vertsThreadFst - 2*edgesThreadFst = 0  =>    */
+/*  =>  vertsThreadFst = 1 +/- sqrt(1 + 4*2*edgesThreadFst), however    */
+/* */
+/* 1 - sqrt(1 + 4*2*edgesThreadFst) is always negative, so              */
+/* vertsThreadFst = 1 + sqrt(1 + 4*2*edgesThreadFst)                    */
+/* */
+/* using (1) we get:                                                    */
+/* vertsThreadFst = 1 + sqrt(1 + 8*(1 * (edges / thread_count)), SO     */
+/* 1ST THREAD USES VERTS FROM 0 to vertsThreadFst - 1                   */
+/* */
+/* vertsThreadSnd = 1 + sqrt(1 + 8*(2 * (edges / thread_count)), SO     */
+/* 2nd thread should(??) use verts from 0 to vertsThreadSnd - 1, BUT    */
+/* from 0 to vertsThreadFst - 1 are already taken by 1st thread, THEN   */
+/* 2ND THREAD USES VERTS FROM vertsThreadFst to vertsThreadSnd - 1      */
+/* */
+/* vertsThreadTrd = 1 + sqrt(1 + 8*(3 * (edges / thread_count)), SO     */
+/* 3rd thread should(??) use verts from 0 to vertsThreadTrd - 1, BUT    */
+/* from 0 to vertsThreadFst - 1 are already taken by 1st thread, AND    */
+/* from vertsThreadFst to vertsThreadSnd -1 taken by 2nd thread, THEN   */
+/* 3RD THREAD USES VERTS FROM vertsThreadSnd to vertsThreadTrd - 1      */
+/* */
+/* vertsThreadFth = 1 + sqrt(1 + 8*(4 * (edges / thread_count)), SO     */
+/* 4th thread should(??) use verts from 0 to vertsThreadFth - 1, BUT    */
+/* from 0 to vertsThreadFst - 1 are already taken by 1st thread, AND    */
+/* from vertsThreadFst to vertsThreadSnd -1 taken by 2st thread, AND    */
+/* from vertsThreadSnd to vertsThreadTrd -1 taken by 3st thread, THEN   */
+/* 4TH THREAD USES VERTS FROM vertsThreadTrd to vertsThreadTrd - 1      */
+/* */
+/* and so on... */
+
+	if (nth == 0) {
+		bgg = 0;
 	} else {
-		end = bodies;
+		bgg = (1 + sqrt(1 + 4*(nth)*edgesPerThread)) / 2.;
 	}
+
+	if (nth == bodies - 1) {
+		end = bodies;
+	} else if (nth > bodies - 1) {
+		bgg = bodies;
+		end = bodies;
+	} else {
+		end = (1 + sqrt(1 + 4*(nth + 1)*edgesPerThread)) / 2.;
+	}
+
+	printf("nth %3.1lli, bgg %3.1i, end %3.1i, verts %3.1i\n",
+		   nth,
+		   2*((bodies - 1) / 2) - end + (bodies % 2 != 1) + 1,
+		   2*((bodies - 1) / 2) - bgg + (bodies % 2 != 1) + 1,
+		   (end * (end - 1) / 2) - (bgg * (bgg - 1) / 2));
+
+	bgg = 2*((bodies - 1) / 2) - bgg + (bodies % 2 != 1) + 1;
+	end = 2*((bodies - 1) / 2) - end + (bodies % 2 != 1) + 1;
+
+	bgg = bgg + end;
+	end = bgg - end;
+	bgg = bgg - end;
 
 	for (i = 0; i < timeSteps; i++) {
 
@@ -128,6 +199,7 @@ int main()
 	if (rslt != 0) {
 		return 1;
 	}
+	edgesPerThread = bodies * (bodies - 1) / (double) thread_count;
 
 	pthread_barrier_init(&barrier, NULL, thread_count);
 
